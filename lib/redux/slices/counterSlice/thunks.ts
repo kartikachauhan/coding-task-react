cd828ee2c728d9065c70ddcae267fe6cf6d28d78
Kartika Chauhan <49581062+kartikachauhan@users.noreply.github.com>
2023-12-15 14:19:51 +0530

    Updated thunks.ts for counter functionality

--- lib/redux/slices/counterSlice/thunks.ts
+++ lib/redux/slices/counterSlice/thunks.ts
@@ -3,6 +3,11 @@ import type { ReduxThunkAction } from "@/lib/redux";
 
 export const incrementIfOddAsync =
   (amount: number): ReduxThunkAction =>
-  () => {
+  (dispatch, getState) => {
     // update only if currentValue is odd
+    const currentValue = getState().counter.value;
+
+    if (currentValue % 2 !== 0) {
+      dispatch(incrementByAmount(amount));
+    }
   };
